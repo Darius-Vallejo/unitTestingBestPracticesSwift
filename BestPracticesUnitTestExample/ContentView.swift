d2eb2ab2abbc34ce5becc35558c6400d9261d527
Darius V <dario.vallejo@rappi.com>
2020-05-31 22:33:12 -0500

    Initial Commit

---
+++ BestPracticesUnitTestExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestPracticesUnitTestExample
+//
+//  Created by dario vallejo on 31/05/20.
+//  Copyright © 2020 dario vallejo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
